fac77757168ac50f2bb0565b21582c29931e7004
Chris Marshall <chris@codegoalie.com>
2023-11-28 10:48:13 -0500

    v1 of Disney countdown image

--- go.mod
+++ go.mod
@@ -5,9 +5,12 @@ go 1.21.4
 require (
 	github.com/fogleman/gg v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.11.3
+	golang.org/x/image v0.14.0
 )
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/labstack/gommon v0.4.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -15,8 +18,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.15.0 // indirect
-	golang.org/x/image v0.14.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,29 +2,106 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"embed"
+	"fmt"
+	"image"
+	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/fogleman/gg"
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/image/font/opentype"
 )
+
+//go:embed assets
+var assets embed.FS
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		LogError:    true,
+		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+				)
+			} else {
+				logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST_ERROR",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("err", v.Error.Error()),
+				)
+			}
+			return nil
+		},
+	}))
+
+	e.GET("/disney-countdown/:days", func(c echo.Context) error {
+		days := c.Param("days")
+
 		dc := gg.NewContext(296, 128)
 
 		// white background
-		dc.DrawRectangle(0, 0, 296, 128)
+		height := 128.0
+		width := 296.0
+		dc.DrawRectangle(0, 0, width, height)
 		dc.SetRGB(1, 1, 1)
 		dc.Fill()
 
-		// red circle
-		dc.DrawCircle(50, 50, 40)
-		dc.SetRGB(1, 0, 0)
-		dc.Fill()
+		// castle icon
+		castleFile, err := assets.Open("assets/cinderella-castle-icon-thumb.jpg")
+		if err != nil {
+			err := fmt.Errorf("opening castle icon: %w", err)
+			return err
+		}
+		im, _, err := image.Decode(castleFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dc.DrawImage(im, 0, 0)
+
+		// countdown text
+		dc.SetRGB(0, 0, 0)
+
+		metropolisTTF, err := assets.ReadFile("assets/Metropolis-Regular.ttf")
+		if err != nil {
+			log.Fatal(err)
+		}
+		metropolisFont, err := opentype.Parse(metropolisTTF)
+		if err != nil {
+			log.Fatal(err)
+		}
+		daysFace, err := opentype.NewFace(
+			metropolisFont,
+			&opentype.FaceOptions{Size: 64, DPI: 72},
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dc.SetFontFace(daysFace)
+		dc.DrawStringAnchored(days, 180, height/2, 0.5, 0.5)
+
+		unitFace, err := opentype.NewFace(
+			metropolisFont,
+			&opentype.FaceOptions{Size: 18, DPI: 72},
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dc.SetFontFace(unitFace)
+		dc.DrawStringAnchored("days", 250, height/2+16, 0.5, 0.5)
 
 		imgBuf := bytes.Buffer{}
-		err := dc.EncodePNG(&imgBuf)
+		err = dc.EncodePNG(&imgBuf)
 		if err != nil {
 			return err
 		}
